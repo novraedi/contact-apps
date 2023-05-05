c4a8bca70fbf2423aaa3701cd968476916d1c699
novraedi <novraepe@gmail.com>
2023-05-05 08:55:30 +0700

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import ContactApp from "./components/ContactApp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles/style.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ContactApp />
   </React.StrictMode>
 );
 
